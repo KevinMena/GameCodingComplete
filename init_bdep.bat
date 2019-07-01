0f58170215fe304f87335ec22a616de075f91e37
Christian Oliveros <christianol_01@hotmail.com>
2019-07-01 10:29:09 -0400

    Removed Build2 Stuff, RIP :(

--- init_bdep.bat
+++
@@ -1,2 +0,0 @@
-bdep init --wipe -C ../KCEngine-release @release cc config.cxx=cl config.cc.coptions=/O2
-bdep init -C ../KCEngine-debug @debug cc config.cxx=cl "config.cc.coptions=/MDd /Od /Zi" config.cc.loptions=/DEBUG
