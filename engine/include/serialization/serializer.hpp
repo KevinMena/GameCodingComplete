e406a5a2530178726cc3d3f813abd2e5beb6468d
Christian Oliveros <christianol_01@hotmail.com>
2019-07-06 15:36:59 -0400

    Serializer: Added length for better performace

--- engine/include/serialization/serializer.hpp
+++ engine/include/serialization/serializer.hpp
@@ -28,11 +28,11 @@ public:
   virtual bool GetText(char *text) = 0;
 
   /*
-  Open an space for a new entry with name and version.
+  Open an space for a new entry with name and version. Length excludes null terminator.
   If it is at the same level as an opened array, the name is not used and the
   entry is appended to the array
   */
-  virtual bool SetEntry(const char *name, s_size version) = 0;
+  virtual bool SetEntry(const char *name, s_size name_length, s_size version) = 0;
   /*
   Gets in the space of an entry and return its version
   If it is at the same level as an opened array, the name is not used and the
@@ -43,11 +43,11 @@ public:
   virtual bool Close() const = 0;
 
   /*
-  Sets a new bool
+  Sets a new bool. Length excludes null terminator.
   If it is at the same level as an opened array, the name is not used and it
   is appended to the array
   */
-  virtual bool SetBool(const char *name, bool value) = 0;
+  virtual bool SetBool(const char *name, s_size name_length, bool value) = 0;
   /*
   Gets if the current/named entry is a bool
   If it is at the same level as an opened array, the name is not used and it
@@ -62,11 +62,11 @@ public:
   virtual bool GetBool(const char *name, bool *result) const = 0;
 
   /*
-  Sets a new unsigned entry
+  Sets a new unsigned entry. Length excludes null terminator.
   If it is at the same level as an opened array, the name is not used and it
   is appended to the array
   */
-  virtual bool SetUint(const char *name, unsigned value) = 0;
+  virtual bool SetUint(const char *name, s_size name_length, unsigned value) = 0;
   /*
   Gets if the current/named entry is an unsigned
   If it is at the same level as an opened array, the name is not used and it
@@ -81,11 +81,11 @@ public:
   virtual bool GetUint(const char *name, unsigned *result) const = 0;
 
   /*
-  Sets a new int entry
+  Sets a new int entry. Length excludes null terminator.
   If it is at the same level as an opened array, the name is not used and it
   is appended to the array
   */
-  virtual bool SetInt(const char *name, int value) = 0;
+  virtual bool SetInt(const char *name, s_size name_length, int value) = 0;
   /*
   Gets if the current/named entry is an int
   If it is at the same level as an opened array, the name is not used and it
@@ -100,11 +100,11 @@ public:
   virtual bool GetInt(const char *name, int *result) const = 0;
 
   /*
-  Sets a new uint64_t entry
+  Sets a new uint64_t entry. Length excludes null terminator.
   If it is at the same level as an opened array, the name is not used and it
   is appended to the array
   */
-  virtual bool SetUint64(const char *name, uint64_t value) = 0;
+  virtual bool SetUint64(const char *name, s_size name_length, uint64_t value) = 0;
   /*
   Gets if the current/named entry is an uint64_t
   If it is at the same level as an opened array, the name is not used and it
@@ -119,11 +119,11 @@ public:
   virtual bool GetUint64(const char *name, uint64_t *result) const = 0;
 
   /*
-  Sets a new int64_t entry
+  Sets a new int64_t entry. Length excludes null terminator.
   If it is at the same level as an opened array, the name is not used and it
   is appended to the array
   */
-  virtual bool SetInt64(const char *name, int64_t value) = 0;
+  virtual bool SetInt64(const char *name, s_size name_length, int64_t value) = 0;
   /*
   Gets if the current/named entry is an int64_t
   If it is at the same level as an opened array, the name is not used and it
@@ -138,11 +138,11 @@ public:
   virtual bool GetInt64(const char *name, int64_t *result) const = 0;
 
   /*
-  Sets a new double entry
+  Sets a new double entry. Length excludes null terminator.
   If it is at the same level as an opened array, the name is not used and it
   is appended to the array
   */
-  virtual bool SetDouble(const char *name, double value) = 0;
+  virtual bool SetDouble(const char *name, s_size name_length, double value) = 0;
   /*
   Gets if the current/named entry is an double
   If it is at the same level as an opened array, the name is not used and it
@@ -157,11 +157,11 @@ public:
   virtual bool GetDouble(const char *name, double *result) const = 0;
 
   /*
-  Sets a new string entry
+  Sets a new string entry. Length excludes null terminator.
   If it is at the same level as an opened array, the name is not used and it
   is appended to the array
   */
-  virtual bool SetString(const char *name, const char *value, s_size size) = 0;
+  virtual bool SetString(const char *name, s_size name_length, const char *value, s_size length) = 0;
   /*
   Gets if the current/named entry is a string
   If it is at the same level as an opened array, the name is not used and it
@@ -182,11 +182,11 @@ public:
   virtual bool GetString(const char *name, const char **value) = 0;
 
   /*
-  Sets a new null entry
+  Sets a new null entry. Length excludes null terminator.
   If it is at the same level as an opened array, the name is not used and it
   is appended to the array
   */
-  virtual bool SetNull(const char *name) = 0;
+  virtual bool SetNull(const char *name, s_size name_length) = 0;
   /*
   Gets if the current/named entry is null
   If it is at the same level as an opened array, the name is not used and it
@@ -195,11 +195,11 @@ public:
   virtual bool IsNull(const char *name) const = 0;
 
   /*
-  Sets a new array entry
+  Sets a new array entry. Length excludes null terminator.
   If it is at the same level as an opened array, the name is not used and it
   is appended to the array
   */
-  virtual bool SetArray(const char *name) = 0;
+  virtual bool SetArray(const char *name, s_size name_length) = 0;
   /*
   Reserve Array memory, might be hard to predict due to recursions
   */
