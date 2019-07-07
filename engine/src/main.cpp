af5a526232ce63b4d78f2f3b4fcb1f83fff09eec
Christian Oliveros <christianol_01@hotmail.com>
2019-07-07 00:45:48 -0400

    Testing JSON Serializer

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -4,6 +4,7 @@
 #include "test/class_test.hpp"
 #include "utils/stopwatch.hpp"
 #include "serialization/serializer.hpp"
+#include "serialization/json_serializer.hpp"
 
 int main(int argc, char **argv) {
 
@@ -36,6 +37,39 @@ int main(int argc, char **argv) {
   printf("Average Total %lf ns\n", static_cast<long double>(total) /  static_cast<long double>(times.size()) );
 
   test.say_msg("Probando");
+
+  Serializer::JsonSerializer ser;
+
+  ser.SetEntry("Memingo", 7, 0);
+
+  ser.SetInt("hola", 4, 42);
+
+  ser.SetArray("array", 5);
+
+  for (size_t i = 0; i < 4; i++)
+  {
+    ser.SetBool(nullptr, 0, (i&1) == 0);
+  }
+
+  ser.CloseArray();
+
+  ser.Close();
+
+  ser.CompilePretty();
+
+  Serializer::s_size size;
+
+  ser.GetSize(&size);
+
+  char* resp = new char[size+1];
+
+  resp[size] = '\0';
+
+  ser.GetText(resp);
+
+  printf("%s\n", resp);
+
+  delete[] resp;
 
   return 0;
 }
