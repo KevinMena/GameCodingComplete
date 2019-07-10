92ecc8d4511298fd0ed146c4cb07a6ec454c4bcb
Christian Oliveros <christianol_01@hotmail.com>
2019-07-09 21:15:36 -0400

    Serialization Test Uint and Bool

--- engine/test_src/serialization/test_serialization.cpp
+++ engine/test_src/serialization/test_serialization.cpp
@@ -15,6 +15,7 @@ protected:
   }
 };
 
+// Register here other serializers
 using serializermethods = ::testing::Types<Serializer::JsonSerializer>;
 
 TYPED_TEST_SUITE(SerializerTest, serializermethods);
@@ -84,6 +85,144 @@ TYPED_TEST(SerializerTest, intType) {
       << "Failed to Parse Compile" << compile;
   EXPECT_TRUE(serializerNormal->GetInt(hola_name.c_str(), &hola_res))
       << test_start_text << "to be an int in Compile";
+  EXPECT_EQ(hola_res, hola) << test_start_text << "in Compile to be " << hola
+                            << " but rather got " << hola_res;
+}
+
+TYPED_TEST(SerializerTest, uintType) {
+  std::unique_ptr<Serializer::ISerializer> serializer(this->GetSerializer());
+
+  const unsigned int hola = 42;
+  const std::string hola_name = "hola";
+  unsigned int hola_res;
+  std::string test_start_text = "Expected \"" + hola_name + "\" ";
+
+  EXPECT_TRUE(serializer->SetUint(hola_name.c_str(), hola_name.length(), hola))
+      << test_start_text << "to be inserted";
+  EXPECT_TRUE(serializer->IsUint(hola_name.c_str()))
+      << test_start_text << "to be an Uint";
+  EXPECT_TRUE(serializer->GetUint(hola_name.c_str(), &hola_res))
+      << test_start_text << "to be an Uint";
+  EXPECT_EQ(hola_res, hola)
+      << test_start_text << "to be " << hola << " but rather got " << hola_res;
+
+  // Pretty Compile
+  EXPECT_TRUE(serializer->CompilePretty()) << "Failed to Pretty Compile";
+
+  Serializer::s_size size;
+
+  EXPECT_TRUE(serializer->GetSize(&size))
+      << "Failed to Get Size of Pretty Compile";
+
+  std::unique_ptr<char[]> resp(new char[size]);
+
+  EXPECT_TRUE(serializer->GetText(resp.get()))
+      << "Failed to Get Pretty Compile";
+
+  std::unique_ptr<Serializer::ISerializer> serializerPretty(
+      this->GetSerializer());
+
+  std::string prettyCompile(resp.get());
+
+  hola_res = 0;
+
+  EXPECT_TRUE(serializerPretty->ParseText(resp.get()))
+      << "Failed to Parse Pretty Compile\n"
+      << prettyCompile;
+  EXPECT_TRUE(serializerPretty->GetUint(hola_name.c_str(), &hola_res))
+      << test_start_text << "to be an Uint in Pretty Compile";
+  EXPECT_EQ(hola_res, hola) << test_start_text << "in Pretty Compile to be "
+                            << hola << " but rather got " << hola_res;
+
+  // Compile
+  EXPECT_TRUE(serializer->Compile()) << "Failed to Compile";
+
+  EXPECT_TRUE(serializer->GetSize(&size)) << "Failed to Get Size of Compile";
+
+  std::unique_ptr<char[]> resp2(new char[size]);
+
+  EXPECT_TRUE(serializer->GetText(resp2.get())) << "Failed to Get Compile";
+
+  std::unique_ptr<Serializer::ISerializer> serializerNormal(
+      this->GetSerializer());
+
+  std::string compile(resp2.get());
+
+  hola_res = 0;
+
+  EXPECT_TRUE(serializerNormal->ParseText(resp2.get()))
+      << "Failed to Parse Compile" << compile;
+  EXPECT_TRUE(serializerNormal->GetUint(hola_name.c_str(), &hola_res))
+      << test_start_text << "to be an Uint in Compile";
+  EXPECT_EQ(hola_res, hola) << test_start_text << "in Compile to be " << hola
+                            << " but rather got " << hola_res;
+}
+
+TYPED_TEST(SerializerTest, boolType) {
+  std::unique_ptr<Serializer::ISerializer> serializer(this->GetSerializer());
+
+  const bool hola = true;
+  const std::string hola_name = "hola";
+  bool hola_res;
+  std::string test_start_text = "Expected \"" + hola_name + "\" ";
+
+  EXPECT_TRUE(serializer->SetBool(hola_name.c_str(), hola_name.length(), hola))
+      << test_start_text << "to be inserted";
+  EXPECT_TRUE(serializer->IsBool(hola_name.c_str()))
+      << test_start_text << "to be an Bool";
+  EXPECT_TRUE(serializer->GetBool(hola_name.c_str(), &hola_res))
+      << test_start_text << "to be an Bool";
+  EXPECT_EQ(hola_res, hola)
+      << test_start_text << "to be " << hola << " but rather got " << hola_res;
+
+  // Pretty Compile
+  EXPECT_TRUE(serializer->CompilePretty()) << "Failed to Pretty Compile";
+
+  Serializer::s_size size;
+
+  EXPECT_TRUE(serializer->GetSize(&size))
+      << "Failed to Get Size of Pretty Compile";
+
+  std::unique_ptr<char[]> resp(new char[size]);
+
+  EXPECT_TRUE(serializer->GetText(resp.get()))
+      << "Failed to Get Pretty Compile";
+
+  std::unique_ptr<Serializer::ISerializer> serializerPretty(
+      this->GetSerializer());
+
+  std::string prettyCompile(resp.get());
+
+  hola_res = !hola;
+
+  EXPECT_TRUE(serializerPretty->ParseText(resp.get()))
+      << "Failed to Parse Pretty Compile\n"
+      << prettyCompile;
+  EXPECT_TRUE(serializerPretty->GetBool(hola_name.c_str(), &hola_res))
+      << test_start_text << "to be an Bool in Pretty Compile";
+  EXPECT_EQ(hola_res, hola) << test_start_text << "in Pretty Compile to be "
+                            << hola << " but rather got " << hola_res;
+
+  // Compile
+  EXPECT_TRUE(serializer->Compile()) << "Failed to Compile";
+
+  EXPECT_TRUE(serializer->GetSize(&size)) << "Failed to Get Size of Compile";
+
+  std::unique_ptr<char[]> resp2(new char[size]);
+
+  EXPECT_TRUE(serializer->GetText(resp2.get())) << "Failed to Get Compile";
+
+  std::unique_ptr<Serializer::ISerializer> serializerNormal(
+      this->GetSerializer());
+
+  std::string compile(resp2.get());
+
+  hola_res = !hola;
+
+  EXPECT_TRUE(serializerNormal->ParseText(resp2.get()))
+      << "Failed to Parse Compile" << compile;
+  EXPECT_TRUE(serializerNormal->GetBool(hola_name.c_str(), &hola_res))
+      << test_start_text << "to be an Bool in Compile";
   EXPECT_EQ(hola_res, hola) << test_start_text << "in Compile to be " << hola
                             << " but rather got " << hola_res;
 }
