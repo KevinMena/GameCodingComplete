839456fc699bd33bff09b5851087cd7e2e8e0862
Christian Oliveros <christianol_01@hotmail.com>
2019-07-10 20:57:28 -0400

    Uint64 Array Test

--- engine/test_src/serialization/test_serialization.cpp
+++ engine/test_src/serialization/test_serialization.cpp
@@ -806,3 +806,98 @@ TYPED_TEST(SerializerTest, int64ArrayType) {
 
   EXPECT_TRUE(serializer->CloseArray());
 }
+
+TYPED_TEST(SerializerTest, uint64ArrayType) {
+  std::unique_ptr<Serializer::ISerializer> serializer(this->GetSerializer());
+
+  const std::vector<uint64_t> hola{42, 7, 0, 1 << 30, (1ll << 50), (1ll << 51)};
+  std::vector<uint64_t> hola_resp;
+  hola_resp.reserve(hola.size());
+  const std::string hola_name = "hola";
+  uint64_t hola_res;
+  std::string test_start_text = "Expected \"" + hola_name + "\" ";
+
+  EXPECT_TRUE(serializer->SetArray(hola_name.c_str(), hola_name.length()));
+
+  EXPECT_TRUE(serializer->SetUint64(nullptr, 0, hola[0]));
+  EXPECT_TRUE(serializer->IsArray(hola_name.c_str()));
+
+  for (size_t i = 1; i < hola.size(); i++) {
+    EXPECT_TRUE(serializer->SetUint64(nullptr, 0, hola[i]));
+  }
+
+  EXPECT_TRUE(serializer->CloseArray());
+
+  // Pretty Compile
+  EXPECT_TRUE(serializer->CompilePretty()) << "Failed to Pretty Compile";
+
+  Serializer::s_size size;
+
+  EXPECT_TRUE(serializer->GetSize(&size))
+      << "Failed to Get Size of Pretty Compile";
+
+  std::unique_ptr<char[]> resp(new char[size]);
+
+  EXPECT_TRUE(serializer->GetText(resp.get()))
+      << "Failed to Get Pretty Compile";
+
+  std::unique_ptr<Serializer::ISerializer> serializerPretty(
+      this->GetSerializer());
+
+  std::string prettyCompile(resp.get());
+
+  EXPECT_TRUE(serializerPretty->ParseText(resp.get()))
+      << "Failed to Parse Pretty Compile\n"
+      << prettyCompile;
+
+  Serializer::s_size array_size;
+
+  EXPECT_TRUE(serializerPretty->IsArray(hola_name.c_str())) << prettyCompile;
+  EXPECT_TRUE(
+      serializerPretty->GetArrayCapacity(hola_name.c_str(), &array_size));
+  ASSERT_EQ(array_size, hola.size()) << "Arrays of Different Size";
+  EXPECT_TRUE(serializerPretty->OpenArray(hola_name.c_str()));
+
+  for (size_t i = 0; i < array_size; i++) {
+    EXPECT_TRUE(serializerPretty->GetUint64(hola_name.c_str(), &hola_res))
+        << test_start_text << "to be an Uint64 in Pretty Compile";
+
+    hola_resp.push_back(hola_res);
+    EXPECT_TRUE(serializerPretty->MoveArray() || i == array_size-1);
+  }
+  EXPECT_TRUE(hola == hola_resp)
+      << " Different Arrays in Pretty Compile\n"
+      << prettyCompile;
+
+  EXPECT_TRUE(serializerPretty->CloseArray());
+
+  // Compile
+  EXPECT_TRUE(serializer->Compile()) << "Failed to Compile";
+
+  EXPECT_TRUE(serializer->GetSize(&size)) << "Failed to Get Size of Compile";
+
+  std::unique_ptr<char[]> resp2(new char[size]);
+
+  EXPECT_TRUE(serializer->GetText(resp2.get())) << "Failed to Get Compile";
+
+  std::unique_ptr<Serializer::ISerializer> serializerNormal(
+      this->GetSerializer());
+
+  std::string compile(resp2.get());
+
+  EXPECT_TRUE(serializer->GetArrayCapacity(hola_name.c_str(), &array_size));
+  ASSERT_EQ(array_size, hola.size()) << "Arrays of Different Size";
+  EXPECT_TRUE(serializer->OpenArray(hola_name.c_str()));
+  hola_resp.clear();
+  for (size_t i = 0; i < array_size; i++) {
+    EXPECT_TRUE(serializer->GetUint64(hola_name.c_str(), &hola_res))
+        << test_start_text << "to be an int in Pretty Compile";
+    hola_resp.push_back(hola_res);
+    EXPECT_TRUE(serializer->MoveArray() || i == array_size-1);
+  }
+  EXPECT_TRUE(hola == hola_resp)
+      << " Different Arrays in Compile\n"
+      << prettyCompile;
+
+  EXPECT_TRUE(serializer->CloseArray());
+}
