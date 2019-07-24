bd0a9586364f18246f9d07b2b51eae851961153a
Christian Oliveros <christianol_01@hotmail.com>
2019-07-24 14:49:41 -0400

    Macro fixes

--- engine/include/os_detection/os_detection.hpp
+++ engine/include/os_detection/os_detection.hpp
@@ -10,35 +10,41 @@ namespace OsDetection
     Based on http://nadeausoftware.com/articles/2012/01/c_c_tip_how_use_compiler_predefined_macros_detect_operating_system
 */
 
+#if defined(_WIN32)
 /* If we are compiling for Windows */
-#define IS_WIN defined(_WIN32)
-
+#define IS_WIN 1
 /* If we are compiling for Windows */
-#ifdef IS_WIN
 constexpr bool kIsWin = true;
 #else // !IS_WIN
+/* If we are compiling for Windows */
+#define IS_WIN 0
+/* If we are compiling for Windows */
 constexpr bool kIsWin = false;
 #endif // IS_WIN
 
 
 
+#if (defined(__APPLE__) && defined(__MACH__))
 /* If we are compiling for Apple */
-#define IS_APPLE (defined(__APPLE__) && defined(__MACH__))
-
+#define IS_APPLE 1
 /* If we are compiling for Apple */
-#ifdef IS_APPLE
 constexpr bool kIsApple = true;
 #else // !IS_APPLE
+/* If we are compiling for Apple */
+#define IS_APPLE 0
+/* If we are compiling for Apple */
 constexpr bool kIsApple = false;
 #endif // IS_APPLE
 
+#if defined(__linux__)
 /* If we are compiling for Linux */
-#define IS_LINUX defined(__linux__)
-
+#define IS_LINUX 1
 /* If we are compiling for Linux */
-#ifdef IS_LINUX
 constexpr bool kIsLinux = true;
 #else // !IS_LINUX
+/* If we are compiling for Linux */
+#define IS_LINUX 0
+/* If we are compiling for Linux */
 constexpr bool kIsLinux = false;
 #endif // IS_LINUX
 
