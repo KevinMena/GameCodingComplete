aeb313d3f82bae27d6841e5f8321e208cf8b1720
Christian Oliveros <christianol_01@hotmail.com>
2019-07-25 21:19:15 -0400

    Windows include config

--- engine/include/os_detection/windows.hpp
+++ engine/include/os_detection/windows.hpp
@@ -1,19 +1,25 @@
 /* Include this file instead of including <Windows.h> directly. */
 
-
 #ifdef NOMINMAX
-#define MINMAX_HPP 0 
+#define MINMAX_HPP 0
 #else // !NOMINMAX
-#define MINMAX_HPP 1 
+#define MINMAX_HPP 1
 #define NOMINMAX 1
 #endif // NOMINMAX
 
 #ifdef STRICT
-#define STRICT_HPP 0 
-#else // !STRICT 
-#define STRICT_HPP 1 
+#define STRICT_HPP 0
+#else // !STRICT
+#define STRICT_HPP 1
 #define STRICT 1
 #endif // STRICT
+
+#ifdef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN_HPP 0
+#else // !WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN_HPP 1
+#define WIN32_LEAN_AND_MEAN 1
+#endif // WIN32_LEAN_AND_MEAN
 
 #include <Windows.h>
 
@@ -25,5 +31,10 @@
 #undef STRICT
 #endif // MINMAX_HPP
 
+#if WIN32_LEAN_AND_MEAN_HPP
+#undef WIN32_LEAN_AND_MEAN
+#endif
+
 #undef MINMAX_HPP
 #undef STRICT_HPP
+#undef WIN32_LEAN_AND_MEAN_HPP
